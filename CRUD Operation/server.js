6b45659b4ca2b78546b01f8a6b9bc8c370aee2c5
HardikSoni1994 <soni.hardik994@gmail.com>
2025-12-09 21:57:32 +0530

    CRUD

--- CRUD Operation/server.js
+++ CRUD Operation/server.js
@@ -2,76 +2,108 @@ const express = require('express');
 const PORT = 4000;
 const app = express();
 
-let allUsers = [
-    {
-        Id: "101",
-        Name: "hardik soni",
-        Email: "soni.hardik@gmail.com",
-        Password: "hardik@123",
-        Phone: 9875124365,
-        Address: "Surat , Gujarat",
-        Age: "23",
-    },
-    {
-        Id: "102",
-        Name: "Rohit Sharma",
-        Email: "rohitsharma@gmail.com",
-        Password: "Rohit@123",
-        Phone: 9876578392,
-        Address: "Mumbai , Maharastra",
-        Age: "36",
-    },
-    {
-        Id: "103",
-        Name: "rahul dravid",
-        Email: "rahuldravid@gmail.com",
-        Password: "rahul@123",
-        Phone: 8765432190,
-        Address: "Ranchi, Jharkhand",
-        Age: "55",
-    },
-    {
-        Id: "104",
-        Name: "Kane Williamson",
-        Email: "kanewilliamson@gmail.com",
-        Password: "Kane@123",
-        Phone: 9696969696,
-        Address: "Tauranga, New Zealand",
-        Age: "65",
-    },
-    {
-        Id: "105",
-        Name: "Ben Stokes",
-        Email: "benstokes@gmail.com",
-        Password: "Stokes@123",
-        Phone: 9595959595,
-        Address: "Christchurch, New Zealand",
-        Age: "46",
-    },
-]
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 
+// Read
+let allUsers = [
+    {
+        id: 1,
+        name: "Hardik",
+        email: "soni.hardik@gmail.com",
+        password: "hardik@123",
+        phone: 9875214514,
+        address: "Surat",
+    },
+    {
+        id: 2,
+        name: "Raj",
+        email: "Raj45@gmail.com",
+        password: "raj@87",
+        phone: 9874100254,
+        address: "Ahmedabad",
+    },
+     {
+        id: 3,
+        name: "kevin",
+        email: "kevin@gmail.com",
+        password: "kevin@89",
+        phone: 9874106540,
+        address: "Bardoli",
+    }
+];
+
+let id = 4;
+
 app.get('/', (req, res) => {
-    res.render('home', {
-        name: "Sujal",
-        isAdmin: true,
-        allUsers,
+    res.render('table', {
+        name: "Hardik",
+        users: allUsers
     });
-})
+});
 
-let id = 106;
-app.post('/addUser', (req, res) => {
-    const user = req.body;
+app.get("/addUser", (req, res) => {
+    res.render("form")
+});
 
-    user.Id = id;
+// app.get("/about", (req, res) => {
+//     res.render('about');
+// });
+
+app.post("/addUser", (req, res) => {
+    // console.log(req.body);    
+    const user = req.query;
+
+    // user.id = Math.floor(Math.random() * 1000);
+    user.id = id;
     id++;
-    console.log(req.query)
-    console.log(user)
-
+ 
     allUsers.push(user);
     res.redirect('/');
+});
+
+app.get("/editPage", (req, res) => {
+    console.log(req.query);
+
+    const user = allUsers.find((user) => user.id == req.query.userId);
+
+    if (!user) {
+        return res.redirect('/not-found');
+    }
+
+    return res.render('edit', {
+        user
+    });
+    
+});
+
+app.post("updateUser", (req, res) => {
+    console.log(req.body);
+
+    allUsers = allUsers.map((user) => {
+        if (user.id == req.body.id) {
+            return req.body;
+        }
+        else{
+            return user;
+        }
+    })
+    return res.redirect('/');
+});
+
+app.get("/deleteUser", (req, res) => {
+    console.log(req.query); // queryString
+    const userId = req.query.userId;
+
+    allUsers = allUsers.filter((user) => user.id != userId);
+
+    console.log(allUsers);
+
+    return res.redirect('/');
+});
+
+app.get('/not-found', (req, res) => {
+    return res.render('not_found');
 })
 
 app.listen(PORT, (err) => {
@@ -79,5 +111,5 @@ app.listen(PORT, (err) => {
         console.log("Server Not Found!!!", err);
         return false;
     }
-    console.log("Server IS Connected PORT Is (http://localhost:4000/)");
+    console.log("Server is Connected PORT Is (http://localhost:4000/)");
 })
