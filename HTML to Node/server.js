663f638ba0d14160d246f669a9747c5881d9d8a2
HardikSoni1994 <soni.hardik994@gmail.com>
2025-12-11 01:37:21 +0530

    HTML to Node.js

---
+++ HTML to Node/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+// Middleware function to check 'name' query parameter
+const middleware = (req, res, next) => {
+    console.log("middleware");
+    if (!req.query.name) {
+        return res.render("checkpage");
+    }
+    if (req.query.name.length > 3) {
+        return next();
+    }
+    else {
+        return res.render("notAccess")
+    }
+    
+}
+
+// call middleware
+app.use(middleware);
+
+app.get("/", (req, res) => {
+    res.render("Homepage", {
+        name: req.query.name || '' });
+})
+
+app.get("/404", (req, res) => {
+    res.render("404");
+})
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(`Error occurred: ${error}`);
+        return false;
+    }
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
